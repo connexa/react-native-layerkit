b677a9981eef38fd96e80635f7456ee98ea612f2
Connexa <info@connexa.io>
2017-09-25 20:42:49 -0400

    Update React Libraries

--- RNLayerKit.h
+++ RNLayerKit.h
@@ -1,4 +1,4 @@
-#import "RCTBridgeModule.h"
+#import <React/RCTBridgeModule.h>
 #import <LayerKit/LayerKit.h>
 #import "JSONHelper.h"
 #import "MessageParts.h"
@@ -6,8 +6,7 @@
 #import "LayerQuery.h"
 #import "RCTLog.h"
 #import "RCTUtils.h"
-#import "RCTEventDispatcher.h"
-//#import "RCTEventEmitter.h"
+#import <React/RCTEventDispatcher.h>
 #import <React/RCTEventEmitter.h>
 
 @interface RNLayerKit : RCTEventEmitter <RCTBridgeModule>
